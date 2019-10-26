a97096c44aefd816a9b6dc5b02ab84e613f35c6f
kenta-nk00 <dokkaebidokkaebir6s@gmail.com>
2019-10-26 04:28:38 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once(__DIR__ . "/Lib/config.php");
+phpinfo();
+ ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>トップ</title>
+</head>
+<body>
+  <a href="./View/signup.php">signup</a>
+  <a href="./View/login.php">login</a>
+</body>
+</html>
